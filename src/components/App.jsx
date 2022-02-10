45160e9da5b32b29a6b1a914264adcae2934b5fe
Lana Ovcharenko <lana.ovchar@gmail.com>
2022-02-10 16:25:00 +0200

    fix details

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -37,7 +37,6 @@ export default function App() {
 
   const countPositiveFeedbackPercentage = () => {
     const total = countTotalFeedback();
-    // const good = this.state.good;
     return Math.round((good * 100) / total);
   };
 
@@ -68,58 +67,3 @@ export default function App() {
       </>
     );
 };
-
-
-//! было
-// class App extends Component {
-//   state = {
-//     good: 0,
-//     neutral: 0,
-//     bad: 0,
-//   };
-
-//   onLeaveFeedback = key => {
-//     this.setState(prevState => ({ [key]: prevState[key] + 1 }));
-//   };
-
-//   countTotalFeedback = () => {
-//     return Object.values(this.state).reduce((sum, value) => sum + value, 0);
-//   };
-
-//   countPositiveFeedbackPercentage = () => {
-//     const total = this.countTotalFeedback();
-//     const good = this.state.good;
-//     return Math.round((good * 100) / total);
-//   };
-
-//   render() {
-//     const { good, neutral, bad } = this.state;
-//     const options = Object.keys(this.state);
-//     const totalFeedback = this.countTotalFeedback();
-//     const positiveFeedback = this.countPositiveFeedbackPercentage(good);
-
-//     return (
-//       <>
-//         <Section title={'Please leave feedback'}>
-//           <FeedbackOptions options={options} onLeaveFeedback={this.onLeaveFeedback} />
-//         </Section>
-
-//         <Section title={'Statistics'}>
-//           {totalFeedback > 0 ? (
-//             <Statistics
-//               good={good}
-//               neutral={neutral}
-//               bad={bad}
-//               total={totalFeedback}
-//               positiveFeedback={positiveFeedback}
-//             />
-//           ) : (
-//             <Notification message="There is no feedback" />
-//           )}
-//         </Section>
-//       </>
-//     );
-//   }
-// }
-
-// export default App;
